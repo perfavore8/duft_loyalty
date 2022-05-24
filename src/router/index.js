0c70b20772429d113ae984f74da74085b7651a7a
Perfavore8 <nekit2353@mail.ru>
2022-05-24 19:55:46 +0200

    second commit

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,12 @@ import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     name: "home",
     component: HomeView,
   },
   {
-    path: "/about",
+    path: "/",
     name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
